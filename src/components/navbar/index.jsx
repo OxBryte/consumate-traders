782841a76d3b69c2ce939c91c944a06eba917ba6
BrightTechnologies1 <silascyrax@gmail.com>
2023-10-30 16:16:01 +0100

    update: navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Box, Button, Flex, HStack, Image, Link, Text, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerOverlay, Flex, HStack, Image, Link, Text, useBreakpointValue, useDisclosure, VStack } from '@chakra-ui/react'
 import { BiMenu } from 'react-icons/bi'
 
 export default function Navbar() {
 
   const isDesktop = useBreakpointValue({ base: false, lg: true })
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <>
@@ -36,11 +37,41 @@ export default function Navbar() {
               <Button bg='brand.300' color='white'>Client Area</Button>
             </HStack>
           ) : (
-            <Box>
+            <Box onclick={onOpen}>
               <BiMenu size={35} />
             </Box>
           )}
         </Flex >
+
+        <Drawer onClose={onClose} isOpen={isOpen} size="full">
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerBody bg="brand.100">
+              <VStack align='center' gap='16px' textDecoration='none'>
+                <Link to='/'>
+                  <Text textDecoration='none'>Trading Models</Text>
+                </Link>
+                <Link to='/'>
+                  <Text>Rules</Text>
+                </Link>
+                <Link to='/'>
+                  <Text>Help Center</Text>
+                </Link>
+                <Link to='/'>
+                  <Text>Affiliate</Text>
+                </Link>
+                <Link to='/'>
+                  <Text>About Us</Text>
+                </Link>
+                <Link to='/'>
+                  <Text>Resources</Text>
+                </Link>
+                <Button bg='brand.300' color='white'>Client Area</Button>
+              </VStack>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
       </Box >
     </>
   )
